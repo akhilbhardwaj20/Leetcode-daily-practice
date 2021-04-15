16649a03834a5a6d0775df1c3377407961991edc
AKHIL BHARDWAJ <44169340+akhilbhardwaj20@users.noreply.github.com>
2021-04-15 15:55:10 +0530

    GFG

---
+++ triplet that sum to a given value.java
@@ -0,0 +1,20 @@
+Problem link ------>     https://practice.geeksforgeeks.org/problems/triplet-sum-in-array-1587115621/1#
+
+public static boolean find3Numbers(int A[], int n, int X) { 
+   
+       Arrays.sort(A);
+       for(int i =0; i < n; i++) {
+           int left = i+1;
+           int right = n-1;
+           while(left < right)
+           if(A[i] + A[left] + A[right] < X) {
+               left++;
+           } else if(A[i] + A[left] + A[right] == X) {
+               return true;
+           } else {
+               right--;
+           }
+          
+       }
+            return false;
+    }
