085725ed36305e58f74cce9349e58e53105f98ae
AKHIL BHARDWAJ <44169340+akhilbhardwaj20@users.noreply.github.com>
2021-04-15 15:58:19 +0530

    Update triplet that sum to a given value.java

--- triplet that sum to a given value.java
+++ triplet that sum to a given value.java
@@ -1,5 +1,6 @@
 Problem link ------>     https://practice.geeksforgeeks.org/problems/triplet-sum-in-array-1587115621/1#
-
+// TC - O(n2)
+// SC - O(1)
 public static boolean find3Numbers(int A[], int n, int X) { 
    
        Arrays.sort(A);
