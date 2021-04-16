f53626be085e3bec85be83d7199b99ffb2621e4d
AKHIL BHARDWAJ <44169340+akhilbhardwaj20@users.noreply.github.com>
2021-04-16 16:57:52 +0530

    GFG

---
+++ Median in a row-wise sorted Matrix.java
@@ -0,0 +1,26 @@
+// Problem link -------->     https://practice.geeksforgeeks.org/problems/median-in-a-row-wise-sorted-matrix1527/1#
+// TC - O(n2)
+// SC - O(1)
+class Solution {
+    int median(int matrix[][], int r, int c) {
+        List<Integer> list = new ArrayList<>();
+        for(int i = 0; i < r; i++) {
+            for(int j = 0; j < c; j++) {
+                list.add(matrix[i][j]);
+            }
+        }
+        int median = 0;
+        Collections.sort(list);
+        int s = list.size();
+        
+        for(int i = 0; i < s; i++) {
+            if(s%2==0) {
+                // for even elements median if the avg of two middle elements
+                median = (list.get(s/2) + list.get(s/2 + 1))/2; 
+            } else {
+                median = list.get(s/2);
+            }
+        }
+        return median;
+    }
+}
